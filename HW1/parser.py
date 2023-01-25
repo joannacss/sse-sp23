9ae896ac17f47067fcfe6531a518c38211fa00c1
Joanna Cecilia da Silva Santos <joanna@esc309008.dhcp.nd.edu>
2023-01-25 13:01:50 -0500

    Mini feed parser

---
+++ HW1/parser.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import gzip
+import tempfile
+
+# Obtain the JSON data
+url = "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.gz"
+response = requests.get(url)
+
+
+# Save the JSON feed in a temporary file
+with tempfile.TemporaryFile() as temp:
+    temp.write(response.content) # save gzip contents to file
+    temp.seek(0) # reset the file handler to the beginning of file
+    with gzip.open(temp, 'rb') as f:
+        file_content = f.read()
+
+        data = json.loads(file_content)
+
+        # Extract the relevant information
+        for entry in data['CVE_Items']:
+            cve_id = entry['cve']['CVE_data_meta']['ID']
+            cve_description = entry['cve']['description']['description_data'][0]['value']
+            print("CVE ID: ", cve_id)
+            print("Description: ", cve_description)
+            
+            for config in entry['configurations']['nodes']:
+                for cpe in config["cpe_match"]:
+                    print("CPE", cpe)
+    
+            
